576db33b0cd9b8b64d292612239f496ea981eba8
Sebastian Krzyszkowiak <dos@dosowisko.net>
2019-01-31 02:40:04 +0100

    maemo5: use "dos" branch for allegro5

--- libsuperderpy-maemo5/build/allegro5/build-allegro5.sh
+++ libsuperderpy-maemo5/build/allegro5/build-allegro5.sh
@@ -5,21 +5,21 @@ sudo /scratchbox/sbin/sbox_ctl start
 sb-conf select FREMANTLE_ARMEL
 sudo /scratchbox/sbin/sbox_sync
 
-wget https://github.com/dos1/allegro5/archive/maemo5.zip -O /scratchbox/users/admin/home/admin/allegro5.zip
+wget https://github.com/dos1/allegro5/archive/dos.zip -O /scratchbox/users/admin/home/admin/allegro5.zip
 
 tee /scratchbox/users/admin/home/admin/build-allegro5.sh << EOF
 #/bin/sh
 set -e
 cd /home/admin
 unzip allegro5.zip
-cd allegro5-maemo5
+cd allegro5-dos
 mkdir -p build
 cd build
 CFLAGS="-mfpu=neon -DGLchar=char" cmake .. -DALLEGRO_SDL=yes -DWANT_DEMO=no -DWANT_EXAMPLES=no
 make -j3
 make install
 cd ../..
-rm -rf allegro5-maemo5 allegro5.zip build-allegro5.sh
+rm -rf allegro5-dos allegro5.zip build-allegro5.sh
 EOF
 
 chmod +x /scratchbox/users/admin/home/admin/build-allegro5.sh
