4631126b5bbb69344c911cadfdc3f47c5c929971
Sebastian Krzyszkowiak <dos@dosowisko.net>
2019-02-06 01:21:12 +0100

    maemo: use the same SDL2 package as other platforms with Maemo specific patches on top

--- libsuperderpy-maemo5/build/sdl2/build-sdl2.sh
+++ libsuperderpy-maemo5/build/sdl2/build-sdl2.sh
@@ -5,24 +5,25 @@ sudo /scratchbox/sbin/sbox_ctl start
 sb-conf select FREMANTLE_ARMEL
 sudo /scratchbox/sbin/sbox_sync
 
-wget https://github.com/dos1/SDL-mirror/archive/maemo5.zip -O /scratchbox/users/admin/home/admin/sdl2.zip
+wget https://dosowisko.net/libsuperderpy/deps/SDL2-2.0.9-p1.tar.gz -O /scratchbox/users/admin/home/admin/sdl2.tar.gz
 
 tee /scratchbox/users/admin/home/admin/build-sdl2.sh <<EOF
 #!/bin/sh
 set -e
 cd /home/admin
-unzip sdl2.zip
-cd SDL-mirror-maemo5
+tar xzf sdl2.tar.gz
+cd SDL2-2.0.9
+cat ../SDL2-patches/*.patch | patch -p1
 mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DEXTRA_CFLAGS="-mfpu=neon -DGLchar=char -DEGLAttrib=intptr_t" -DVIDEO_X11_XINERAMA=OFF
 make -j3
 make install
 cd ../..
-rm -rf SDL-mirror-maemo5 sdl2.zip build-sdl2.sh
+rm -rf SDL2-2.0.9 sdl2.tar.gz build-sdl2.sh
 EOF
 
 chmod +x /scratchbox/users/admin/home/admin/build-sdl2.sh
 /scratchbox/login /home/admin/build-sdl2.sh
-rm /home/admin/build-sdl2.sh
-
+sudo rm -rf /home/admin/build-sdl2.sh
+sudo rm -rf /scratchbox/users/admin/home/admin/SDL2-patches
