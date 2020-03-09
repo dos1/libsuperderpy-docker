2e4863c8ea77b822132fc7d7fdb61708f35ec204
Sebastian Krzyszkowiak <dos@dosowisko.net>
2020-03-09 01:30:36 +0100

    Update toolchains and versions of dependencies

--- libsuperderpy-maemo5/build/sdl2/build-sdl2.sh
+++ libsuperderpy-maemo5/build/sdl2/build-sdl2.sh
@@ -5,14 +5,14 @@ sudo /scratchbox/sbin/sbox_ctl start
 sb-conf select FREMANTLE_ARMEL
 sudo /scratchbox/sbin/sbox_sync
 
-wget https://dosowisko.net/libsuperderpy/deps/SDL2-2.0.9-p2.tar.gz -O /scratchbox/users/admin/home/admin/sdl2.tar.gz
+wget https://dosowisko.net/libsuperderpy/deps/SDL2-2.0.10.tar.gz -O /scratchbox/users/admin/home/admin/sdl2.tar.gz
 
 tee /scratchbox/users/admin/home/admin/build-sdl2.sh <<EOF
 #!/bin/sh
 set -e
 cd /home/admin
 tar xzf sdl2.tar.gz
-cd SDL2-2.0.9
+cd SDL2-2.0.10
 cat ../SDL2-patches/*.patch | patch -p1
 mkdir -p build
 cd build
@@ -20,7 +20,7 @@ cmake .. -DCMAKE_BUILD_TYPE=Release -DEXTRA_CFLAGS="-mfpu=neon -DGLchar=char -DE
 make -j3
 make install
 cd ../..
-rm -rf SDL2-2.0.9 sdl2.tar.gz build-sdl2.sh
+rm -rf SDL2-2.0.10 sdl2.tar.gz build-sdl2.sh
 EOF
 
 chmod +x /scratchbox/users/admin/home/admin/build-sdl2.sh
