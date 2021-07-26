98dc30a5aad3f66397026ec3ab0214c87ae47a9a
Sebastian Krzyszkowiak <dos@dosowisko.net>
2021-07-26 08:21:04 +0200

    Update dependencies

--- libsuperderpy-maemo5/build/allegro5/build-allegro5.sh
+++ libsuperderpy-maemo5/build/allegro5/build-allegro5.sh
@@ -5,21 +5,21 @@ sudo /scratchbox/sbin/sbox_ctl start
 sb-conf select FREMANTLE_ARMEL
 sudo /scratchbox/sbin/sbox_sync
 
-wget https://dosowisko.net/libsuperderpy/deps/allegro-5.2.6.0-p1.tar.gz -O /scratchbox/users/admin/home/admin/allegro5.tar.gz
+wget https://dosowisko.net/libsuperderpy/deps/allegro-5.2.7.0-p1.tar.gz -O /scratchbox/users/admin/home/admin/allegro5.tar.gz
 
 tee /scratchbox/users/admin/home/admin/build-allegro5.sh << EOF
 #/bin/sh
 set -e
 cd /home/admin
 tar xzf allegro5.tar.gz
-cd allegro-5.2.6.0
+cd allegro-5.2.7.0
 mkdir -p build
 cd build
-CFLAGS="-mfpu=neon -DGLchar=char" cmake .. -DALLEGRO_SDL=yes -DWANT_DEMO=no -DWANT_EXAMPLES=no
+CFLAGS="-mfpu=neon -DGLchar=char" cmake .. -DALLEGRO_SDL=yes -DWANT_DEMO=no -DWANT_EXAMPLES=no -DWANT_PHYSFS=no
 make -j3
 make install
 cd ../..
-rm -rf allegro-5.2.6.0 allegro5.tar.gz build-allegro5.sh
+rm -rf allegro-5.2.7.0 allegro5.tar.gz build-allegro5.sh
 EOF
 
 chmod +x /scratchbox/users/admin/home/admin/build-allegro5.sh
